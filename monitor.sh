2259b95a9486321378f635938c76cc173caa8054
Ayaan <am3855@njit.edu>
2026-02-24 12:12:08 -0500

    monitor file change

--- monitor.sh
+++ monitor.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# Log the date and memory usage
 
-echo "Memory Log - $(date)" >> system_log.txt
-free -h | grep Mem >> system_log.txt
-echo "--------------------------------" >> system_log.txt
+echo "SYSTEM REPORT (Memory) - $(date)" >> /home/amod/Lab4/system_log.txt
+/usr/bin/free -h | /bin/grep Mem >> /home/amod/Lab4/system_log.txt
+echo "--------------------------------" >> /home/amod/Lab4/system_log.txt
